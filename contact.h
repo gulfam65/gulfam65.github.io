7bcc2598dc2946c6e3e81579ffbb67accbfa6471
gulfam sen <142521138+gulfam65@users.noreply.github.com>
2023-08-27 09:23:36 +0530

    Update and rename contact.md to contact.h

---
+++ contact.h
@@ -0,0 +1,37 @@
+tml<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Contact Us - SSAS Public School</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="gallery">Gallery</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <section id="contact">
+            <h2>Contact Us</h2>
+            <p>For inquiries and information, please feel free to contact us using the following details:</p>
+            <ul>
+                <li>Email: contact@ssaspublicschool.me</li>
+                <li>Phone: +1 (123) 456-7890</li>
+                <li>Address: 123 School Street, City, Country</li>
+            </ul>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2023 SSAS Public School</p>
+    </footer>
+</body>
+</html>
